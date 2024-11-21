db6e97281971adf67ce43e9f2176fdb338cb5fb5
John <johnfarris217@gmail.com>
2024-11-20 21:20:52 -0600

    add semantic ui and NavBar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { Menu, Button } from "semantic-ui-react";
+
+function NavBar() {
+    return (
+        <Menu className="navbar" style={{ backgroundColor: "#f9f9f9" }}>
+            <Menu.Item>Home</Menu.Item>
+        </Menu>
+    );
+}
+
+export default NavBar;
