8e0716b7f3af285baa7335b8c28766ee20b8610a
John <johnfarris217@gmail.com>
2024-11-23 10:30:20 -0600

    fix App errors from adding login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import NavBar from "./components/NavBar";
-import { Outlet } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { UserContext } from "./contexts/UserContext";
 
 function App() {
+    const { user, setUser, setAuthorizing } = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setAuthorizing(true);
+        fetch("/me").then((resp) => {
+            if (resp.ok) {
+                resp.json().then((user) => {
+                    setUser(user);
+                    setAuthorizing(false);
+                });
+            } else {
+                setAuthorizing(false);
+            }
+        });
+    }, [setUser]);
+
+    function onLogin(user) {
+        setUser(user);
+        navigate("/profile");
+    }
+
     return (
         <div className="App">
             <br />
             <NavBar />
             <br />
-            <Outlet />
+            <Outlet
+                context={{
+                    onLogin: onLogin,
+                    user: user,
+                    setUser: setUser,
+                }}
+            />
         </div>
     );
 }
