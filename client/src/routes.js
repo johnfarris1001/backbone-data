34ebcce56499f9aaca4c43da9e236300fd5780d2
John <johnfarris217@gmail.com>
2024-11-23 08:39:51 -0600

    add error page to react app

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,12 @@
 import App from "./App";
+import ErrorPage from "./components/ErrorPage";
+import Login from "./components/Login";
 
 const routes = [
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorPage />,
     },
 ];
 
